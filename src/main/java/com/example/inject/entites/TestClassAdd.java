3661b76579f1830b0a7a68a7a4875c476559d24d
777 <goszakupki.aktiv@gmail.com>
2023-08-26 21:31:59 +0300

    DRE-0001: is 1 2

--- src/main/java/com/example/inject/entites/TestClassAdd.java
+++ src/main/java/com/example/inject/entites/TestClassAdd.java
@@ -36,9 +36,9 @@ public class TestClassAdd {
 
 
     private void hello88(){
-        System.out.println("8822");
+        System.out.println("8822 is first");
     }
     private void hello99(){
-        System.out.println("9922");
+        System.out.println("9922 is second");
     }
 }
