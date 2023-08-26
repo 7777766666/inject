f55e0f5374c4b70772ddfbf35a93455033b46feb
777 <goszakupki.aktiv@gmail.com>
2023-08-26 19:20:00 +0300

    DRE-0001: HELLO World!!!!!!!!!!!!

--- src/main/java/com/example/inject/entites/TestClassAdd.java
+++ src/main/java/com/example/inject/entites/TestClassAdd.java
@@ -35,7 +35,10 @@ public class TestClassAdd {
     private Integer newPrice;
 
 
-    private void hello(){
-        System.out.println("9999");
+    private void hello4(){
+        System.out.println("4");
+    }
+    private void hello5(){
+        System.out.println("5");
     }
 }
